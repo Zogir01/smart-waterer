92ed2b0c3cfae0ddffa528a42f85c0c40a6cd43d
Tomasz Wojtasek <tom3k2002w@gmail.com>
2025-06-08 21:37:50 +0200

    08-06-2025 - changed config.h "layout" - added TO-DO comment in wifi_ap.h

--- esp32_firmware/main/inc/config.h
+++ esp32_firmware/main/inc/config.h
@@ -3,43 +3,46 @@
 
 // ------------------------------ KONFIGURACJA OGÓLNA --------------------------------------
 
-#define ESP_INTR_FLAG_DEFAULT       	0           // Flaga dla obslugi przerwań
+#define ESP_INTR_FLAG_DEFAULT       		0           	// Flaga dla obslugi przerwań
 
 // ------------------------------ GPIO -----------------------------------------------------
 
-#define GPIO_PUMP_CONTROL               32      	// Pin do włączania/wyłączania pompy
-#define GPIO_MANUAL_WATERING_BUTTON     21      	// Pin przyjmujacy stan z przycisku
-#define GPIO_HUM_SENSOR             	34          // Pin podpięty do czujnika wilgotności
-#define GPIO_SDA                    	18          // Pin danych I2C
-#define GPIO_SCL                    	5          	// Pin zegara I2C
-#define GPIO_RST                    	-1          // Pin reset I2C
+#define GPIO_PUMP_CONTROL               	32      		// Pin do włączania/wyłączania pompy
+#define GPIO_MANUAL_WATERING_BUTTON     	21      		// Pin przyjmujacy stan z przycisku
+#define GPIO_HUM_SENSOR             		34          	// Pin podpięty do czujnika wilgotności
+#define GPIO_SDA                    		18          	// Pin danych I2C
+#define GPIO_SCL                    		5          		// Pin zegara I2C
+#define GPIO_RST                    		-1          	// Pin reset I2C
 
 // ------------------------------ KONFIGURACJA LCD -----------------------------------------
 
 //#define LCD_PIXEL_CLOCK_HZ        (400 * 1000)
-#define LCD_H_RES                   	128         // Szerokość wyświetlacza
-#define LCD_V_RES                   	32          // Wysokość wyświetlacza
+#define LCD_H_RES                   		128         	// Szerokość wyświetlacza
+#define LCD_V_RES                   		32          	// Wysokość wyświetlacza
 
 // ------------------------------ DOMYSLNA KONFIGURACJA ------------------------------------
-#define DEFAULT_CONFIG_WATERING_TIME				1000		// Czas podlewania
-#define DEFAULT_CONFIG_SAMPLE_COUNT          		20          // Ilość pomiarów wilgotności do uśrednienia
-#define DEFAULT_CONFIG_READ_DELAY          			100         // Czas w ms pomiędzy pomiarami wilgotności
-#define DEFAULT_CONFIG_DRY_THRESHOLD          		50          // Próg wilgotności dla którego podlewać
+
+#define DEFAULT_CONFIG_WATERING_TIME		1000			// Czas podlewania
+#define DEFAULT_CONFIG_SAMPLE_COUNT         20      		// Ilość pomiarów wilgotności do uśrednienia
+#define DEFAULT_CONFIG_READ_DELAY          	100     		// Czas w ms pomiędzy pomiarami wilgotności
+#define DEFAULT_CONFIG_DRY_THRESHOLD        50      		// Próg wilgotności dla którego podlewać
 
 // ------------------------------ WALIDACJA DANYCH -----------------------------------------
-#define MIN_WATERING_TIME							100
-#define MAX_WATERING_TIME							10000
 
-#define MIN_SAMPLE_COUNT							1
-#define MAX_SAMPLE_COUNT							10000
+#define MIN_WATERING_TIME					100				// Minimalny
+#define MAX_WATERING_TIME					10000			// Maksymalny czas podlewania
 
-#define MIN_READ_DELAY 								50
-#define MAX_READ_DELAY 								10000
+#define MIN_SAMPLE_COUNT					1				// Minimalna
+#define MAX_SAMPLE_COUNT					10000			// Maksymalna ilość pomiarów wilgotności do uśrednienia
 
-#define MIN_DRY_THRESHOLD							1
-#define MAX_DRY_THRESHOLD 							99
+#define MIN_READ_DELAY 						50				// Minimalny
+#define MAX_READ_DELAY 						10000			// Maksymalny czas w ms pomiędzy pomiarami wilgotności
+
+#define MIN_DRY_THRESHOLD					1				// Minimalny
+#define MAX_DRY_THRESHOLD 					99				// Maksymalny próg wilgotności dla którego podlewać
 
 // ------------------------------ DODATKOWE DO TESTÓW --------------------------------------
-#define HUM_THRESHOLD_REVERSE       				true        // Określa czy podlewanie odbędzie się poniżej progu czy powyżej (do testów)
+
+#define HUM_THRESHOLD_REVERSE       		true    		// Określa czy podlewanie odbędzie się poniżej progu czy powyżej (do testów)
 
 #endif // CONFIG_H
--- esp32_firmware/main/inc/wifi_ap.h
+++ esp32_firmware/main/inc/wifi_ap.h
@@ -1,6 +1,15 @@
 #ifndef WIFI_AP_H
 #define WIFI_AP_H
 
+/**
+ * @brief [Tutaj wstawić skrócony opis funkcji]
+ * 
+ * [Tutaj wstawić opis funkcji]
+ * 
+ * @param [Tutaj wstawić opis parametrów (jeśli są)]
+ * @param [Tutaj wstawić opis parametrów (jeśli są)]
+ * @return [Tutaj wstawić opis co funkcja zwraca.]
+ */
 void wifi_init_softap(void);
 
 #endif // WIFI_AP_H
